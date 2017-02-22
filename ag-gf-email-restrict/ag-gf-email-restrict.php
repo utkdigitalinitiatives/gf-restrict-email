30d0e75ff2050e1e255e95ef689757b3b1ad2444
Dan Greene <dgreene@utk.edu>
2017-02-22 15:20:12 -0500

    Plugin frame started

---
+++ ag-gf-email-restrict/ag-gf-email-restrict.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Ag - Gravity Forms Restrict to @utk.edu or @tennessee.edu email addressess
+Description: Based on code from David Smith https://gravitywiz.com/banlimit-email-domains-for-gravity-form-email-fields/
+Version: 1.0
+Author: Dan Greene
+*/
+
+?>
